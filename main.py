8184086430a20050d86d56dd05da3cca5584a216
Roc136 <1361050885@qq.com>
2021-04-24 15:40:53 +0800

    fix color

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def make_graph(percent: float):
     done_block = '█'
     empty_block = '█'
     pc_rnd = round(percent)
-    return f"<a style=\"color:black\">{done_block * int(pc_rnd / 4)}</a><a style=\"color:grey\">{empty_block * int(25 - int(pc_rnd / 4))}</a>"
+    return f"<font color=\"black\">{done_block * int(pc_rnd / 5)}</font><font color=\"grey\">{empty_block * int(20 - int(pc_rnd / 5))}</font>"
 
 
 def make_list(data: list):
@@ -168,7 +168,7 @@ def make_list(data: list):
     for l in data[:5]:
         # ln = len(l['name'])
         # ln_text = len(l['text'])
-        op = f"<tr><td width=\"25%\">{l['name'][:25]}</td><td width=\"25%\">{l['text']}</td><td width=\"50%\">{make_graph(l['percent'])}   {l['percent']}%</td></tr>"
+        op = f"<tr><td width=\"25%\">{l['name'][:25]}</td><td width=\"25%\">{l['text']}</td><td>{make_graph(l['percent'])} {l['percent']}%</td></tr>"
         data_list.append(op)
     return ' \n'.join(data_list)
 
@@ -180,7 +180,7 @@ def make_commit_list(data: list):
         # ln = len(l['name'])
         # ln_text = len(l['text'])
         # op = f"{l['name']}{' ' * (13 - ln)}{l['text']}{' ' * (15 - ln_text)}{make_graph(l['percent'])}   {l['percent']}%"
-        op = f"<tr><td width=\"25%\">{l['name']}</td><td width=\"25%\">{l['text']}</td><td width=\"50%\">{make_graph(l['percent'])}   {l['percent']}%</td></tr>"
+        op = f"<tr><td width=\"25%\">{l['name']}</td><td width=\"25%\">{l['text']}</td><td>{make_graph(l['percent'])} {l['percent']}%</td></tr>"
         data_list.append(op)
     return ' \n'.join(data_list)
 
